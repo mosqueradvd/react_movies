c15d0fffacd327c7b0c902bf41af8d6b5e83be2b
David Mosquera <mosqueradavid@outlook.es>
2021-02-17 08:51:17 -0500

    Add final changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,63 @@
 import { useState, useEffect } from 'react'
-import './App.css'
+import './styles/App.css'
+import Navbar from '../src/components/Navbar'
+import Skeleton from '../src/components/Skeleton'
 
 function App() {
   const [movies, setMovies] = useState([])
-  const [id, setId] = useState(1)
-  const [movieId, setMovieId] = useState()
+  const [page, setPage] = useState(1)
 
   const nextPage = async () => {
-    setId((id) => id + 1)
+    setPage((page) => page + 1)
   }
 
   useEffect(() => {
     async function getMovies() {
       const result = await fetch(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=fcd48ca74e06f52d29422c2007f44c82&language=en-US&page=${id}`
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=fcd48ca74e06f52d29422c2007f44c82&language=en-US&page=${page}`
       )
       const data = await result.json()
       setMovies((movies) => [...movies, ...data.results])
-      // console.log('movies >>>', movies[0].id)
     }
 
-    console.log('length >>>', movies)
-    // for (let i = 0; i < movies.length; i++) {
-    //   setMovieId(movies[i].id)
-    // }
-
-    // async function getMovieBanner() {
-    //   await fetch(
-    //     `https://api.themoviedb.org/3/movie/${movieId}/images?api_key=fcd48ca74e06f52d29422c2007f44c82&52AfXWuXCHn3UjD17rBruA9f5qb.jpg`
-    //   )
-    // }
-
-    // getMovieBanner()
     getMovies()
-  }, [])
+  }, [page])
 
   return (
     <div className='App'>
-      {movies.map((item, index) => (
-        <div key={index}>
-          <img src={movieId} alt='movie banner'></img>
-          <h3>{item.title}</h3>
-        </div>
-      ))}
-      <button onClick={nextPage}>Next page</button>
+      <Navbar />
+      <div className='App-container'>
+        {!movies.length ? (
+          <Skeleton />
+        ) : (
+          movies.map((item, index) => (
+            <div key={index} className='App-card'>
+              <div>
+                <img
+                  className='App-card--img'
+                  src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
+                  alt='movie banner'
+                ></img>
+              </div>
+              <h2 className='App-card--title'>{item.title}</h2>
+              <div className='App-card--info'>
+                <h3>Language {item.original_language}</h3>
+                <h3>Release date {item.release_date}</h3>
+                <h3>Rating {item.vote_average}</h3>
+              </div>
+              <div className='App-card--paragraph'>
+                <h2>Movie Overview</h2>
+                <p>{item.overview}</p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      <div className='App-button--container'>
+        <button className='App-button' onClick={nextPage}>
+          Load more movies!
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import '../styles/components/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <img
+          src='https://trabajaconnosotros.decathlon.co/images/logo.png'
+          alt=''
+        />
+        <h1>Movie Center</h1>
+        <ul>
+          <li>
+            <img
+              src='https://randomuser.me/api/portraits/women/95.jpg'
+              alt=''
+            />
+          </li>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
